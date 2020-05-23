c6d1cc9d1fb79c21a315b716b1b205323476ef7d
HugoMG <32292019+hugo3011mendez@users.noreply.github.com>
2020-05-23 16:50:08 +0200

    Viendo como se cambia desde GitHub

--- Hola.java
+++ Hola.java
@@ -1,6 +1,6 @@
 class Hola {
  public static void main(String[] args){
- System.out.println("Bienvenido al mundo de Java");
+ System.out.println("Bienvenido al mundo de Java modificado desde GitHub");
  System.out.println("I have no more branches to commit, said the Ent");
  }
 }
